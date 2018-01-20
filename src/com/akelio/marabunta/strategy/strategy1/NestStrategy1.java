0d71f6606f0fc3aa6b48c99d13ec300358018ded
Augustin Delale <adelale@hotmail.com>
2018-01-20 16:03:51 +0100

    ...

--- src/com/akelio/marabunta/strategy/strategy1/NestStrategy1.java
+++ src/com/akelio/marabunta/strategy/strategy1/NestStrategy1.java
@@ -12,13 +12,33 @@ public class NestStrategy1 extends NestStrategy {
 		
 		int t = mem.getM0()+1;
 		
-		
 		switch(t) {
 			case 1:_newAnt(1);
 			case 2:_antOut(1,0,1,0);
 
 			case 3:_newAnt(2);
 			case 4:_antOut(2,0,1,0);
+
+			case 5:_newAnt(3);
+			case 6:_antOut(3,0,1,0);
+
+			case 7:_newAnt(4);
+			case 8:_antOut(4,0,1,0);
+
+			case 9:_newAnt(5);
+			case 10:_antOut(5,0,1,0);
+
+			case 11:_newAnt(6);
+			case 12:_antOut(6,0,1,0);
+
+			case 13:_newAnt(7);
+			case 14:_antOut(7,0,1,0);
+
+			case 15:_newAnt(8);
+			case 16:_antOut(8,0,1,0);
+
+			case 17:_newAnt(9);
+			case 18:_antOut(9,0,1,0);
 		}
 		
 		setMemory(t);
