ac5ebdff9520696e5ca930c410b19774e20356e5
Augustin Delale <adelale@hotmail.com>
2018-01-20 13:18:04 +0100

    ...

---
+++ src/com/akelio/marabunta/strategy/ant/AntStrategy.java
@@ -0,0 +1,8 @@
+package com.akelio.marabunta.strategy.ant;
+
+import com.akelio.marabunta.input.ant.InputAnt;
+
+public interface AntStrategy {
+
+	public void process(InputAnt input);
+}
---
+++ src/com/akelio/marabunta/strategy/nest/NestStrategy.java
@@ -0,0 +1,8 @@
+package com.akelio.marabunta.strategy.nest;
+
+import com.akelio.marabunta.input.nest.InputNest;
+
+public interface NestStrategy {
+
+	public void process(InputNest input);
+}
