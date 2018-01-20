9c3f4ea63160a27fff5c331306d600909fc3f4b6
Carre Quentin <qcarre@akelio.fr>
2018-01-20 14:26:02 +0100

    no message

--- src/com/akelio/marabunta/strategy/NestStrategy.java
+++ src/com/akelio/marabunta/strategy/NestStrategy.java
@@ -1,8 +1,30 @@
 package com.akelio.marabunta.strategy;
 
+import com.akelio.marabunta.Const;
 import com.akelio.marabunta.input.nest.InputNest;
 
 public abstract class NestStrategy {
 
 	public abstract void process(InputNest input);
+
+	private void p(String line) {
+		System.out.println(line);
+	}
+
+	protected void newAnt(int type) {
+		p(Const.NA_ANT_NEW + " " + type);
+	}
+
+	protected void antOut(int type, int food, int m0, int m1) {
+		p(Const.NA_ANT_OUT + " " + type + " " + food + " " + m0 + " " + m1);
+	}
+
+	protected void setMemory(int... memory) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 20; i++) {
+			sb.append(memory.length < i ? memory[i] : 0);
+			sb.append(" ");
+		}
+		p(Const.NA_SET_MEMORY + " " + sb.toString().trim());
+	}
 }
