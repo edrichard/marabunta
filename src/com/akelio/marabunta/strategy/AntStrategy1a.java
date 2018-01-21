01baa065008baaf02e928b2ba2d65807b96bc82b
Augustin Delale <adelale@hotmail.com>
2018-01-21 01:21:42 +0100

    ...

--- src/com/akelio/marabunta/strategy/AntStrategy1a.java
+++ src/com/akelio/marabunta/strategy/AntStrategy1a.java
@@ -1,6 +1,8 @@
 package com.akelio.marabunta.strategy;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.akelio.marabunta.Debug;
@@ -20,7 +22,7 @@ public class AntStrategy1a extends AntStrategy {
 		
 		Pheromone myNearestPh = getMyNearestPheromone(input);
 		List<Pheromone> myPheromones = getMyPheromones(input);
-		List<Pheromone> pathPheromones = getPathPheromones(input);
+		List<Pheromone> pathPheromones = getSortedPathPheromones(input);
 
 		int t = mem.getM0();
 		int r = mem.getM1();
@@ -76,42 +78,69 @@ public class AntStrategy1a extends AntStrategy {
 		
 		// rechercher de la nourriture
 		
-		
-		
 		// on place une pheromone tous les 3 tours
 		if(t%3==0) {
 			setMemory(t,r);
 			_putPheromone(input.getType()*10);
 			return;
+		}	
+				
+		if(bestFood!=null && bestFood.isNear()) {
+			// nouriture a portee
+
+			int amount = Math.min(bestFood.getAmount(), input.getStockLeft());
+			if(amount>0) {
+				// il y a de la nourriture a recolter : on collecte
+				setMemory(t,r);
+				_collect(bestFood.getId(), amount);
+				return;
+			}
 		}
-
-		List<Food> foods = input.getFoods();
-
-		if(foods.isEmpty()) {
-			// aucune nouriture en vue : on explore
-			setMemory(t,r);
-			_exploreD(3);
-			
-//			if(t>400) {
-//				suicide();
-//				return;
-//			}
-			return;
-		}
-
-		if(!bestFood.isNear()) {
+		
+		if(bestFood!=null && bestFood.isFar()) {
 			// nouriture en vue : on se rapproche
 			setMemory(t,r);
 			_moveTo(bestFood.getId());
 			return;
 		}
 		
-		// nouriture a portee
+		
+		if(!pathPheromones.isEmpty()) {
+			if(pathPheromones.size()==1) {
+				setMemory(t,r);
+				_moveTo(pathPheromones.get(0).getId());
+				return;
+			}
+			
+			Pheromone ph1 = pathPheromones.get(0);
+			Pheromone ph2 = pathPheromones.get(1);
+			
+			if((ph1.getId()%10==9 && ph2.getId()%10==1)) {
+				setMemory(t,r);
+				_moveTo(ph2.getId());
+				return;
+			}
+			
+			if(ph1.getId()%10==1 && ph2.getId()%10==9) {
+				setMemory(t,r);
+				_moveTo(ph1.getId());
+				return;
+			}
+			
+			if((ph1.getId()%10 > ph2.getId()%10)) {
+				setMemory(t,r);
+				_moveTo(ph1.getId());
+				return;
+			}
+			
+			if((ph1.getId()%10 < ph2.getId()%10)) {
+				setMemory(t,r);
+				_moveTo(ph2.getId());
+				return;
+			}
 
-		int amount = Math.min(bestFood.getAmount(), input.getStockLeft());
-		if(amount>0) {
-			// il y a de la nourriture a recolter : on collecte
-			_collect(bestFood.getId(), amount);
+			setMemory(t,r);
+			_explore();
 			return;
 		}
 	}
@@ -146,10 +175,26 @@ public class AntStrategy1a extends AntStrategy {
 		return input.getType()*10;
 	}
 	
-	public List<Pheromone> getPathPheromones(InputAnt input) {
+	
+	public List<Pheromone> getSortedPathPheromones(InputAnt input) {
+		if (input.getPheromones().isEmpty()) return null;
+		
 		List<Pheromone> list = new ArrayList<>();
-		for(Pheromone pheromone : input.getPheromones())  if(pheromone.getType()>511)
-			list.add(pheromone);
+		for(Pheromone pheromone : input.getPheromones())  {
+			if(pheromone.getType()%10>0) list.add(pheromone);
+		}
+		Collections.sort(list,new Comparator<Pheromone>() {
+			public int compare(Pheromone o1, Pheromone o2) {
+				int d1 = o1.getDist();
+				int d2 = o2.getDist();
+				if(d1<d2) return 1;
+				if(d1>d2) return -1;
+				return 0;
+			}
+		});
+		
 		return list;
 	}
+	
+	
 }
