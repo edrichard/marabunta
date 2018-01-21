5735305461b774ae22090fb1c34114063fdaa716
Augustin Delale <adelale@hotmail.com>
2018-01-21 02:04:22 +0100

    ...

--- src/com/akelio/marabunta/strategy/NestStrategy1.java
+++ src/com/akelio/marabunta/strategy/NestStrategy1.java
@@ -8,6 +8,7 @@ import com.akelio.marabunta.input.nest.NestMemory;
 public class NestStrategy1 extends NestStrategy {
 	
 	public static final int ANT_MAXNB = 20;
+	public static final int ANT_MAXNB2 = 60;
 	public static final int STOCK_MIN = 50;
 	
 
@@ -15,8 +16,15 @@ public class NestStrategy1 extends NestStrategy {
 
 		Debug.d("NestStrategy1:process");
 		NestMemory mem = input.getMemory();
-		
 		int t = mem.getM0();
+		int c = mem.getM1();
+		
+		if(!input.getAntIns().isEmpty()) {
+			c++;
+			setMemory(t+1,c);
+		}
+		
+		int maxCreation = c==0 ? ANT_MAXNB : ANT_MAXNB2;
 		
 		AntCount antCount = input.getExistingAntType();
 		if(antCount!=null && antCount.getQuantity()>0) {
@@ -24,10 +32,10 @@ public class NestStrategy1 extends NestStrategy {
 			_antOut(antCount.getType(),5,0,0);
 			return;
 		}
-
-		if(t<ANT_MAXNB && input.getStock()>STOCK_MIN) {
+		
+		if(t<maxCreation && input.getStock()>STOCK_MIN) {
 			Debug.d("_newAnt");
-			setMemory(t+1);
+			setMemory(t+1,c);
 			_newAnt(t);
 			return;
 		}
