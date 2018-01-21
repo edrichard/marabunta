3a3e149e0bb49b8ed8d724061a9c62407e7f011d
Augustin Delale <adelale@hotmail.com>
2018-01-21 04:56:13 +0100

    ...

--- src/com/akelio/marabunta/Main1.java
+++ src/com/akelio/marabunta/Main1.java
@@ -1,11 +1,11 @@
 package com.akelio.marabunta;
 
-import com.akelio.marabunta.strategy.AntStrategy3;
+import com.akelio.marabunta.strategy.AntStrategy1;
 import com.akelio.marabunta.strategy.NestStrategy1;
 
 public class Main1 {
 
 	public static void main(String[] args) {
-		new Program(new AntStrategy3(),new NestStrategy1());
+		new Program(new AntStrategy1(),new NestStrategy1());
 	}
 }
